e5228e71e7d62705b3a2a1f51ed5b6a987a17235
ShAndriy <andriyshchudlo@gmail.com>
2016-11-30 12:02:09 +0200

    add txt reader method in class ReadFromFile

--- src/com/marriagemodule/ReaderFromFile.java
+++ src/com/marriagemodule/ReaderFromFile.java
@@ -1,7 +1,38 @@
 package com.marriagemodule;
 
-/**
- * Created by andriis on 11/30/16.
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReaderFromFile {
+
+    public static void main(String[] args) {
+        ReaderFromFile reader = new ReaderFromFile();
+
+       reader.txtReader();
+
+
+    }
+    private void txtReader(){
+        List<String> listDateOfPersons = new ArrayList<String>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("/home/andriis/Documents/persons.txt"));
+            String line;
+            while ((line = br.readLine())!=null){
+                listDateOfPersons.add(line);
+
+            }
+
+        }catch (Exception e){
+            System.out.println(e);
+
+        }
+        System.out.println(listDateOfPersons);
+    }
+
 }
+
+
+
